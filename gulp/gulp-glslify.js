b253d975e66d032c63b5a5b4e20826d9dc6a1ac6
Timmy Kokke <mental_c@hotmail.com>
2021-08-28 22:52:56 +0200

    added Gulp again

---
+++ gulp/gulp-glslify.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const glsl = require('glslify');
+const through = require('through2');
+ 
+module.exports = (options = {}) => {
+    return through.obj(function(file, encoding, callback) {
+        if (file.isDirectory() || file.isNull() || file.isStream()) {
+            return callback(null, file);
+        }
+
+        var content = String(file.contents);
+        options.basedir = options.basedir || path.dirname(file.path);
+        content = glsl(content, options);
+        const fullname = file.path.split(/\/|\\/gm).pop();
+        const name = fullname.split('.')[0];
+
+        content =`window.${name} = ${JSON.stringify(content)};`;
+
+        file.contents = Buffer.from(content);
+        file.path += '.js';
+
+        this.push(file);
+        callback();
+    });
+};
---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const terser = require('gulp-terser');
+const glslify = require('./gulp/gulp-glslify');
+const gulpif = require('gulp-if');
+const gulpCopy = require('gulp-copy');
+const webp = require('gulp-webp');
+const inlinesource = require('gulp-inline-source');
+const htmlmin = require('gulp-htmlmin');
+
+function isJavaScript(file) {
+    // Check if file extension is '.js'
+    return file.extname === '.js';
+}
+
+function isShader(file) {
+    return file.extname === '.glsl';
+}
+
+function javascript(cb) {
+    {
+        return gulp.src(['./src/classes/*.js', './src/shaders/*.glsl'])
+
+            .pipe(sourcemaps.init())
+            //   .pipe(gulpif(isJavaScript, terser()))
+            .pipe(gulpif(isShader, glslify()))
+            .pipe(concat('main.js'))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('./dist/'));
+    }
+}
+
+function copyStatic(cb) {
+    return gulp.src('./src/static/*.html')
+        .pipe(gulpCopy('./dist', { prefix: 3 }));
+}
+
+function doWebp(cb){
+    return gulp.src('./src/static/*.png')
+        .pipe(webp({lossless:true}))
+        .pipe(gulp.dest('./dist'));
+}
+
+gulp.task('watch', function () {
+    return gulp.watch(['./src/components/*.js','./src/static/*.*', './src/shaders/*.glsl'],
+        { ignoreInitial: false },
+        gulp.series(copyStatic, doWebp, javascript));
+});
+
+function production() {
+    return gulp.src(['./src/classes/*.js', './src/shaders/*.glsl'])
+        //   .pipe(sourcemaps.init())
+        .pipe(gulpif(isJavaScript, terser({
+            ecma:2020,
+            compress:{
+                unsafe:true,
+                unsafe_Function:true,
+                unsafe_arrows:true,
+                unsafe_comps:true,
+                unsafe_math:true,
+                unsafe_methods:true,
+                unsafe_proto:true,
+                unsafe_regexp:true,
+                unsafe_symbols:true,
+                unsafe_undefined:true,
+                passes:10
+            }
+        })))
+        .pipe(gulpif(isShader, glslify()))
+        .pipe(concat('main.js'))      
+        .pipe(gulp.dest('./dist/'));
+};
+
+gulp.task('inlinesource', function () {
+    return gulp.src('./dist/*.html')
+        .pipe(htmlmin({collapseWhitespace:true}))
+        .pipe(inlinesource({saveRemote:false}))
+        .pipe(gulp.dest('./dist/'));
+});
+
+exports.default = gulp.series(copyStatic,doWebp, javascript);
+exports.copy = copyStatic;
+exports.prod =  gulp.series(copyStatic, doWebp, production);
