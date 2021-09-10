cde2694cdf3f498a3a807b55827529a450821774
Timmy Kokke <mental_c@hotmail.com>
2021-09-10 15:39:10 +0200

    camera rotations

--- src/classes/teleportCamera.js
+++ src/classes/teleportCamera.js
@@ -1,11 +1,20 @@
+/// <reference path="../../typings/playcanvas.d.ts" />
 let TeleportCamera = pc.createScript('teleportCamera');
 
 TeleportCamera.prototype.initialize = function () {
-    this.app.on('teleport:to', function (position) {
-       
+    this.tempVec = new pc.Vec3();
+
+    this.app.on('teleport:to', function (position, yrotation = null) {
         let targetPos = new pc.Vec3(Math.floor(position.x) + .5, 0, Math.floor(position.z) + .5);
         this.entity.setPosition(targetPos);
 
+        if (yrotation != null) {
+            this.tempVec.copy(this.app.mainCamera.getLocalPosition());
+            this.entity.translateLocal(this.tempVec);
+            this.entity.setLocalEulerAngles(0, yrotation, 0);
+            // @ts-ignore engine-tsd
+            this.entity.translateLocal(this.tempVec.scale(-1));
+        }
     }, this);
 };
 
